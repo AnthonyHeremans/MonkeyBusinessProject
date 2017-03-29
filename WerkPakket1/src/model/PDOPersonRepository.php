9b2be6429bd231a219ba2f4df67f50c61f2595b6
Bryan <bryan.Deferm@gmail.com>
2017-03-29 20:10:23 +0200

    PDOPersonRepo.php aangepast

--- WerkPakket1/src/model/PDOPersonRepository.php
+++ WerkPakket1/src/model/PDOPersonRepository.php
@@ -21,7 +21,7 @@ class PDOPersonRepository implements PersonRepository
     public function findPersonById($id )
     {
         try {
-            $statement = $this->connection->prepare('SELECT * FROM person WHERE id=?');
+            $statement = $this->connection->prepare('SELECT * FROM person WHERE person_id=?');
             $statement->bindParam(1, $id, \PDO::PARAM_INT);
             $statement->execute();
             $results = $statement->fetchAll(\PDO::FETCH_ASSOC);
@@ -35,5 +35,55 @@ class PDOPersonRepository implements PersonRepository
             return null;
         }
     }
-}
+
+    public function findPersonByFirstName($firstname)
+    {
+        try {
+            $statement = $this->connection->prepare('SELECT * FROM person WHERE person_firstname=?');
+            $statement->bindParam(1, $id, \PDO::PARAM_INT);
+            $statement->execute();
+            $results = $statement->fetchAll(\PDO::FETCH_ASSOC);
+
+            if (count($results) > 0) {
+                return new Person($results[0]['id'], $results[0]['firstname'], $results[0]['lastname'], $results[0]['event']);
+            } else {
+                return null;
+            }
+        } catch (\Exception $exception) {
+            return null;
+        }
+    }
+
+    public function findPersonByLastName($lastname)
+    {
+        try {
+            $statement = $this->connection->prepare('SELECT * FROM person WHERE person_lastname=?');
+            $statement->bindParam(1, $id, \PDO::PARAM_INT);
+            $statement->execute();
+            $results = $statement->fetchAll(\PDO::FETCH_ASSOC);
+
+            if (count($results) > 0) {
+                return new Person($results[0]['id'], $results[0]['firstname'], $results[0]['lastname'], $results[0]['event']);
+            } else {
+                return null;
+            }
+        } catch (\Exception $exception) {
+            return null;
+        }
+    }
+
+    public function addPerson(Person $person)
+    {
+        // TODO : anthony moet hier nog iets toevoegen.
+    }
+
+    public function findAllPersons()
+    {
+        // TODO: Implement findAllPersons() method.
+    }
+
+    public function removeOnID($id)
+    {
+        // TODO: Implement removeOnID() method.
+    }
 }
