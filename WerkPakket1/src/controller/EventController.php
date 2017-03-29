e2d78658c2e1a23681158a3de34e1c8f34cb27c0
Anthony Heremans <anthony_heremans@hotmail.com>
2017-03-29 16:17:28 +0200

    sync

--- WerkPakket1/src/controller/EventController.php
+++ WerkPakket1/src/controller/EventController.php
@@ -6,7 +6,19 @@
  * Date: 29/03/2017
  * Time: 16:14
  */
+namespace controller;
+
+use model\EventRepository;
+use view\View;
 class EventController
 {
 
+    public function __construct(PersonRepository $personRepository, View $view)
+    {
+        $this->personRepository = $personRepository;
+        $this->view = $view;
+    }
+
+    private $personRepository;
+    private $view;
 }
