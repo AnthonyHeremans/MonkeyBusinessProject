c996e065f676cd562336bd939fcb704beab9bee1
Anthony Heremans <anthony_heremans@hotmail.com>
2017-03-30 13:21:00 +0200

    sync

---
+++ WerkPakket1/src/controller/EventController.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: 11401671
+ * Date: 29/03/2017
+ * Time: 16:14
+ */
+namespace controller;
+
+use model\EventRepository;
+use view\View;
+class EventController
+{
+    private $eventRepository;
+    private $view;
+
+    public function __construct(EventRepository $eventRepository, View $view)
+    {
+        $this->eventRepository = $eventRepository;
+        $this->view = $view;
+    }
+
+    public function handleFindEventById($id = null)
+    {
+        $event = $this->eventRepository->findEventById($id);
+        $this->view->show(['event' => $event]);
+    }
+    public function handleFindEventByName($name = null)
+    {
+        $event = $this->eventRepository->findEventByName($name);
+        $this->view->show(['event' => $event]);
+    }
+    /*public function handleFindAllEvents($name = null)
+    {
+        $event = $this->eventRepository->findEventByName($name);
+        $this->view->show(['event' => $event]);
+    }
+    public function handelAddEvent($name = null)
+    {
+        $event = $this->eventRepository->findEventByName($name);
+        $this->view->show(['event' => $event]);
+    }
+    public function handeRemoveEvent($name = null)
+    {
+        $event = $this->eventRepository->findEventByName($name);
+        $this->view->show(['event' => $event]);
+    }*/
+//$event = $this->eventRepository->findEventByName($name);
+}
